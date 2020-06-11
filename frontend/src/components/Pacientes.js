89707361908de87373bc28a9ab239efd31b77803
danicelistobon <danielct-97@hotmail.com>
2020-06-10 21:49:24 -0500

    added pacientes component

---
+++ frontend/src/components/Pacientes.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Pacientes = ({ citas }) => {
+  if (citas.length === 0) {
+    return null;
+  }
+  return (
+    <>
+      <h1 className='my-5'>Administrador de Pacientes</h1>
+      <div className='container mt-5 py-5'>
+        <div className='row'>
+          <div className='col-12 mb-5 d-flex justify-content-center'>
+            <Link to='/nueva' className='btn btn-success text-uppercase py-2 px-5 font-weight-bold'>Crear cita</Link>
+          </div>
+          <div className='col-md-8 mx-auto'>
+            <div className='list-group'>
+              {citas.map(cita => (
+                <Link to={`/cita/${cita._id}`} key={cita._id} className='p-5 list-group-item list-group-item-action flex-column align-items-start'>
+                  <div className='d-flex w-100 justify-content-between mb-4'>
+                    <h3 className='mb-3'>{cita.nombre}</h3>
+                    <small className='fecha-alta'>
+                      {cita.fecha} - {cita.hora}
+                    </small>
+                  </div>
+                  <p className='mb-0'>
+                    {cita.consulta}
+                  </p>
+                  <div className='contacto py-3'>
+                    <p>Propietario: {cita.propietario}</p>
+                    <p>Teléfono: {cita.phone}</p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Pacientes;
